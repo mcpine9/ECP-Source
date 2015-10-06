7a84ee2030b80208dd3937e94e6d0bdf76790a58
Pine, Mark <Mark@intuitivespace.com>
2015-10-06 16:38:03 -0500

    Added Bootstrap, Angularjs and RequireJS; Renamed ECP project;

--- ECP/Global.asax
+++ ECP/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECP.WebApiApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECP_API.WebApiApplication" Language="C#" %>
--- ECP/Properties/AssemblyInfo.cs
+++ ECP/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ECP")]
+[assembly: AssemblyTitle("ECP_API")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
